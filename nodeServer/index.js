99386e69234f0b333fee05ce1b424c02983bc0cb
Shaban <lcs2020044@iiitl.ac.in>
2023-08-16 22:52:46 +0530

    first commit

---
+++ nodeServer/index.js
@@ -0,0 +1,23 @@
+
+
+
+// Node server that will handle sockeet io connection
+const io = require('socket.io')(8000)
+
+const user = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        console.log("New User", name);
+        user[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', user[socket.id]);
+        delete user[socket.id];
+    });
+}) 
